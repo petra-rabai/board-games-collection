46d7916c4c2e0a44f817a6bae3635e19ecb54b59
Petra Rábai <petra.rabai@icloud.com>
2022-07-16 15:45:29 +0200

    .

---
+++ BoardGamesCollection/Games.Logic.RPSLS/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace Games.Logic.RPSLS
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Value { get; set; }
+	}
+}
---
+++ BoardGamesCollection/Games.Logic.RPSLS/Items.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games.Logic.RPSLS
+{
+	public class Items
+	{
+		public enum Rock
+		{
+			Scissor,
+			
+		}
+
+		public enum Scissor
+		{
+			Paper,
+			Lizard
+		}
+
+		public enum Lizard
+		{
+			Spock,
+			Paper
+		}
+
+		public enum Spock
+		{
+			Rock,
+			Scissor
+		}
+
+		public enum Paper
+		{
+			Rock,
+			Spock
+		}
+
+	}
+}
